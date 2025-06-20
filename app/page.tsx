c151ed2c4ada4a225f4a32c704463bb5dcf11c04
Rochita <Venkateshrochita@gmail.com>
2025-06-20 13:21:17 +0530

    Fourth Commit Test code

--- app/page.tsx
+++ app/page.tsx
@@ -2,85 +2,60 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="maincontainer" style={{ paddingBottom: "100px" }}>
-      <main
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+    <div className="h-screen p-10">
+      {/* Logo */}
+      <div>
         <Image
           src="/next.svg"
-          alt="logo next"
+          alt="next logo"
           width="180"
           height="38"
-          className="invertIfDark"
-        ></Image>
+        />
+      </div>
 
-        <div>
-          <ol style={{ textAlign: "center", listStyleType: "decimal" }}>
-            <li>
-              Start editing <span style={{ background: "#eee" }}>app/page.tsx</span>
-            </li>
-            <li style={{ marginTop: "10px" }}>Save the file to see the result.</li>
-          </ol>
-        </div>
+      {/* Text Section */}
+      <div>
+        <p>To start edit the file:</p>
+        <p>
+          <code>app/page.tsx</code>
+        </p>
+        <p>Save it to see changes</p>
+      </div>
 
-        <div className="buttons" style={{ display: "flex", gap: 10, flexDirection: "column" }}>
-          <a
-            href="https://vercel.com/new"
-            style={{
-              backgroundColor: "black",
-              color: "white",
-              padding: "10px",
-              borderRadius: "20px",
-              textDecoration: "none",
-            }}
-          >
-            <Image src="/vercel.svg" alt="vercel" width={20} height={20}></Image> Deploy
-          </a>
+      {/* Buttons */}
+      <div className="flex flex-col sm:flex-row gap-5 mt-10">
+        <a
+          href="https://vercel.com/new"
+          target="_blank"
+          className="bg-black text-white p-2 rounded"
+        >
+          <Image src="/vercel.svg" alt="vercel" width="20" height="20" />
+          Deploy Now
+        </a>
 
-          <a
-            href="https://nextjs.org/docs"
-            style={{
-              border: "1px solid gray",
-              padding: "10px",
-              borderRadius: "20px",
-              display: "inline-block",
-            }}
-          >
-            Docs
-          </a>
-        </div>
-      </main>
+        <a
+          href="https://nextjs.org/docs"
+          target="_blank"
+          className="border border-gray-300 p-2 rounded"
+        >
+          Read Docs
+        </a>
+      </div>
 
-      <footer
-        style={{
-          marginTop: "100px",
-          textAlign: "center",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-          flexWrap: "wrap",
-          gap: 20,
-        }}
-      >
+      {/* Footer Links */}
+      <div style={{ marginTop: 100 }}>
         <a href="https://nextjs.org/learn">
-          <Image src="/file.svg" width="16" height="16" alt="icon" />
-          Learn More
+          <Image src="/file.svg" width={16} height={16} alt="file" /> Learn
         </a>
-
+        <br />
         <a href="https://vercel.com/templates">
-          <Image src="/window.svg" width="16" height="16" alt="icon" />
-          Templates
+          <Image src="/window.svg" width={16} height={16} alt="win" /> Examples
         </a>
-
+        <br />
         <a href="https://nextjs.org">
-          <Image src="/globe.svg" width="16" height="16" alt="icon" />
-          nextjs.org
+          <Image src="/globe.svg" width={16} height={16} alt="globe" /> Visit
         </a>
-      </footer>
+      </div>
     </div>
   );
 }
