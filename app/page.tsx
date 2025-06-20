15234d60f2be08c64a37a9a33876fa2c5fd56d50
Rochita <Venkateshrochita@gmail.com>
2025-06-20 13:33:01 +0530

    Messy Code Pull Request

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,63 @@
-import Image from 'next/image'
+import Image from "next/image"
 
-export default function HomePage() {
+export default function HomepageComponent() {
+
   return (
-    <div className="page-container">
-      <h1>Welcome to Next js!</h1>
+    <div class="my-container" style={{ minHeight: "100vh", padding: "30px" }}>
+      
+      <center>
+        <h2>nextjs welcome page</h2>
+      </center>
 
-      <div className="logo-place" style={{ textAlign: 'center' }}>
-        <Image
-          src="/next.svg"
-          alt="next image"
-          width="180px"
-          height="38px"
+      <div>
+        <Image 
+          src="/next.svg" 
+          alt="Logo" 
+          width="180" 
+          height="38" 
+          class="invert-if-dark" 
         />
       </div>
 
       <div>
-        <ul>
+        <ol type="1">
           <li>Edit the file <b>app/page.tsx</b></li>
-          <li>See the change after saving.</li>
-        </ul>
+          <li>Save to see output</li>
+        </ol>
       </div>
 
-      <div className="buttonsArea" style={{ display: 'flex', gap: 20, flexWrap: 'wrap' }}>
-        <a
-          href="https://vercel.com"
-          target="_blank"
-          className="vercel-button"
-        >
-          <Image src="/vercel.svg" width="20px" height="20px" alt="vercel logo" />
-          <span>Deploy now</span>
+      <div class="btns" style={{ display: "flex", flexDirection: "row", marginTop: "40px", gap: "15px" }}>
+        <a href="https://vercel.com" target="_blank">
+          <button style={{ background: "#000", color: "#fff", borderRadius: "50px", padding: "10px 20px" }}>
+            <Image src="/vercel.svg" alt="vercel" width={20} height={20} />
+            deploy
+          </button>
         </a>
 
-        <a
-          href="https://nextjs.org/docs"
-          className="docs-link"
-        >
-          Documentation
+        <a href="https://nextjs.org/docs" target="_blank">
+          <button style={{ border: "1px solid gray", padding: "10px", borderRadius: "50px" }}>
+            go docs
+          </button>
         </a>
       </div>
 
-      <footer className="bottom-footer" style={{ marginTop: '80px' }}>
-        <p>Useful Links:</p>
-        <a href="https://nextjs.org/learn">
-          <Image src="/file.svg" width={16} height={16} alt="file" />
-          Learn Next
-        </a>
-        <br />
-        <a href="https://vercel.com/templates">
-          <Image src="/window.svg" width={16} height={16} alt="template" />
-          Templates
-        </a>
-        <br />
-        <a href="https://nextjs.org">
-          <Image src="/globe.svg" width={16} height={16} alt="globe" />
-          Visit Site
-        </a>
+      <hr style={{ marginTop: "60px", marginBottom: "20px" }} />
+
+      <footer>
+        <div>
+          <p>More Links Below 👇</p>
+          <a href="https://nextjs.org/learn">
+            <Image src="/file.svg" width="16" height="16" alt="file" /> Learn
+          </a><br />
+          <a href="https://vercel.com/templates">
+            <Image src="/window.svg" width="16" height="16" alt="template" /> Templates
+          </a><br />
+          <a href="https://nextjs.org">
+            <Image src="/globe.svg" width="16" height="16" alt="globe" /> Visit next
+          </a>
+        </div>
       </footer>
+
     </div>
   )
 }
